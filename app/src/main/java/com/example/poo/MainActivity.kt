ee701eb4c01848d8352fd2978597417735090d17
Marcelo Moll <mollmarcelomoll@gmail.com>
2022-10-15 19:14:29 -0300

    15-10-2022 Almacenar Valor de Try Catch

--- app/src/main/java/com/example/poo/MainActivity.kt
+++ app/src/main/java/com/example/poo/MainActivity.kt
@@ -8,8 +8,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun value_try (a: Int, b: Int): Any {
+            var res =
+                try {
+                    println("Division $a / $b")
+                    a / b
+                } catch (e: Exception) {
+                    println("Vamos a manejar este error")
+                    "Division no permitida"
+                }
+            return  res
+        }
+
         var persona: Persona = Persona("Marcelo",30)
         persona.propiedad = 99
         println(persona.propiedad)
+
+        var res1 = value_try(10,2)
+        println(res1)
+        var res2 = value_try(10,0)
+        println(res2)
     }
 }
